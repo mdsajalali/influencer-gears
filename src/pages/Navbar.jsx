05863bebaf4ad40fc565b57d4ecae8f19938a683
mdsajalali <mdsajalali2020@gmail.com>
2023-11-03 21:31:07 +0600

    navbar responsive added

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -8,35 +8,44 @@ import navItems from "../../public";
 const Navbar = () => {
   const [click, setClick] = useState(false);
   return (
-    <div className="max-w-[1200px] mx-auto p-10">
-      <div className="md:flex items-center justify-between  ">
+    <div className="mx-auto max-w-[1200px] p-10">
+      <div className="flex items-center justify-between md:flex">
         <div className="flex justify-between">
           <div
-            className="text-3xl cursor-pointer md:hidden "
+            className="cursor-pointer text-3xl md:hidden "
             onClick={() => setClick(!click)}
           >
-            {click ? <AiOutlineClose /> : <HiOutlineMenuAlt2 />}
+            {click ? (
+              <AiOutlineClose className="text-2xl" />
+            ) : (
+              <HiOutlineMenuAlt2 />
+            )}
           </div>
-          <h1 className="text-2xl font-bold">Influencer products</h1>
+          <h1 className="hidden text-2xl  font-bold md:block">
+            Influencer products
+          </h1>
         </div>
         <nav
-          className={`md:flex items-center list-none gap-5 absolute top-20 md:static  ${
-            click ? "left-10" : "left-[-20%] md:left-0"
+          className={`absolute top-20 list-none items-center gap-5 rounded-lg px-24 py-10 text-center shadow-lg   md:static md:left-[40%] md:flex md:rounded-none md:p-0   md:shadow-none ${
+            click ? "" : "left-[-100%] md:left-0"
           }`}
         >
           {navItems.map((item) => (
-            <li key={item.id}>
+            <li key={item.id} className="my-5 md:my-0 ">
               <a
-                className="text-[17px] hover:text-[#E02C6D] transition-all  "
+                className="text-[17px]  transition-all hover:text-[#E02C6D]   "
                 href={item.path}
               >
                 {item.name}
               </a>
             </li>
           ))}
-          <FiSearch className="text-2xl" />
-          <BsCartPlus className="text-3xl" />
+          <div className="flex items-center gap-4">
+            <FiSearch className="text-2xl " />
+            <BsCartPlus className="text-3xl" />
+          </div>
         </nav>
+        <BsCartPlus className="text-3xl md:hidden" />
       </div>
     </div>
   );

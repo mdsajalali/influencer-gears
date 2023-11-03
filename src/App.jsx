0d220b0032a3a5772f1a1fab713a2ae580731866
mdsajalali <mdsajalali2020@gmail.com>
2023-11-03 18:23:37 +0600

    initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-center bg-slate-300 text-2xl'>Influencer Gears</h1>
+    </div>
+  )
+}
+
+export default App

97569e2efa0b19051dd3d8007c61a3982623a874
mdsajalali <mdsajalali2020@gmail.com>
2023-11-08 20:25:07 +0600

    footer section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import FeaturedProduct from "./pages/FeaturedProduct";
+import Footer from "./pages/Footer";
 import Hero from "./pages/Hero";
 import Navbar from "./pages/Navbar";
 import Products from "./pages/Products";
@@ -6,13 +7,16 @@ import Sponsors from "./pages/Sponsors";
 
 const App = () => {
   return (
-    <div className="mx-auto max-w-[1200px] p-10">
-      <Navbar />
-      <Hero />
-      <Sponsors />
-      <Products />
-      <FeaturedProduct />
-    </div>
+    <>
+      <div className="mx-auto max-w-[1200px] p-10">
+        <Navbar />
+        <Hero />
+        <Sponsors />
+        <Products />
+        <FeaturedProduct />
+      </div>
+      <Footer />
+    </>
   );
 };
 
---
+++ src/pages/Footer.jsx
@@ -0,0 +1,26 @@
+import {
+  AiFillLinkedin,
+  AiFillYoutube,
+  AiOutlineInstagram,
+  AiOutlineTwitter,
+} from "react-icons/ai";
+
+const Footer = () => {
+  const year = new Date().getFullYear();
+  return (
+    <div className="bg-[#0A0826] py-14 text-center text-white">
+      <h1 className="text-2xl">Influencer products</h1>
+      <p className="my-5 text-[13px] font-light">
+        Copyright © {year} influencer products <br /> All rights reserved
+      </p>
+      <div className="flex items-center justify-center gap-5">
+        <AiOutlineInstagram className="h-8 w-8 cursor-pointer rounded-full bg-slate-500 p-[5px] " />
+        <AiFillLinkedin className="h-8 w-8 cursor-pointer rounded-full bg-slate-500 p-[5px] " />
+        <AiOutlineTwitter className="h-8 w-8 cursor-pointer rounded-full bg-slate-500 p-[5px] " />
+        <AiFillYoutube className="h-8 w-8 cursor-pointer rounded-full bg-slate-500 p-[5px] " />
+      </div>
+    </div>
+  );
+};
+
+export default Footer;

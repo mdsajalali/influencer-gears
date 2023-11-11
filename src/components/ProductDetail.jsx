5a448b295abae26d25445098dd41747a97a01c17
mdsajalali <mdsajalali2020@gmail.com>
2023-11-11 20:24:55 +0600

    product page fixed

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -8,12 +8,13 @@ const ProductDetail = () => {
   const idInt = parseInt(id);
   // console.log(typeof idInt);
 
-  const ProductItem = product.find((item) => item.id === idInt);
-  console.log(ProductItem);
+  const ProductItem = Array.isArray(product)
+    ? product.find((item) => item.id === idInt)
+    : null;
 
   return (
     <div className="mx-auto max-w-[1200px] p-10">
-      <h1 className="text-center mt-20 text-2xl">Product: 0{id}</h1>{" "}
+      <h1 className="mt-20 text-center text-2xl">Product: 0{id}</h1>{" "}
       <Link to="/">
         <button className="mt-3 rounded-md bg-[#E02C6D] p-2 text-[14px] font-semibold   text-white transition-all hover:bg-[#af2d5d]">
           Go To Home Page
@@ -22,11 +23,11 @@ const ProductDetail = () => {
       <div className="mx-auto mb-10 mt-5 max-w-[500px] rounded-lg p-3 shadow-xl">
         <img
           className="mt-3 w-full"
-          src={ProductItem.img}
-          alt={ProductItem.title}
+          src={ProductItem?.img}
+          alt={ProductItem?.title}
         />
-        <h1 className="my-2 text-2xl">{ProductItem.title}</h1>
-        <h2>${ProductItem.price}</h2>
+        <h1 className="my-2 text-2xl">{ProductItem?.title}</h1>
+        <h2>${ProductItem?.price}</h2>
         <h3 className="flex items-center">
           <div className="my-1 flex  text-[#FB8200]">
             <AiFillStar />
@@ -35,11 +36,11 @@ const ProductDetail = () => {
             <AiFillStar />
             <BsStarHalf />
           </div>
-          <div className="my-1 ml-5">{ProductItem.rating}</div>
+          <div className="my-1 ml-5">{ProductItem?.rating}</div>
         </h3>
-        <p>{ProductItem.shifting}</p>
+        <p>{ProductItem?.shifting}</p>
         <p className="my-1">{status}</p>
-        <p>{ProductItem.possibility}</p>
+        <p>{ProductItem?.possibility}</p>
       </div>
     </div>
   );

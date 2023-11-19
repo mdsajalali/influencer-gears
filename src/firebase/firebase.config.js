b201745e28c3376cea60f6426f1319a1f3736a58
mdsajalali <mdsajalali2020@gmail.com>
2023-11-19 15:35:01 +0600

    env file added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,17 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDpswQ7QXfXZevhLaBylSalj-uxnvuQHD8",
-  authDomain: "influencer-products.firebaseapp.com",
-  projectId: "influencer-products",
-  storageBucket: "influencer-products.appspot.com",
-  messagingSenderId: "1065944789638",
-  appId: "1:1065944789638:web:f21e14ed31112dbc499707",
+const firebaseConfig = { 
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGEINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
